66c695643dc209115274d009b083eecd4b498544
sush <sushantsingh4720@gmail.com>
2022-05-02 16:19:57 +0530

    add new features

--- index.js
+++ index.js
@@ -8,53 +8,39 @@ for (var i = 0; i < buttonlength; i++) {
     buttonAnimation(buttonInnerHTML);
   }
 }
-document.addEventListener("keypress",function(event){
-makesound(event.key);
-buttonAnimation(event.key);
+document.addEventListener("keypress", function (event) {
+  makesound(event.key);
+  buttonAnimation(event.key);
 });
 function makesound(key) {
-  key=key.toLowerCase();
+  key = key.toLowerCase();
   switch (key) {
     case "w":
-      var tom1 = new Audio(
-        "/my web code/Drum Kit Starting Files/sounds/tom-1.mp3"
-      );
+      var tom1 = new Audio("sounds/tom-1.mp3");
       tom1.play();
       break;
     case "a":
-      var tom2 = new Audio(
-        "/my web code/Drum Kit Starting Files/sounds/tom-2.mp3"
-      );
+      var tom2 = new Audio("sounds/tom-2.mp3");
       tom2.play();
       break;
     case "s":
-      var tom3 = new Audio(
-        "/my web code/Drum Kit Starting Files/sounds/tom-3.mp3"
-      );
+      var tom3 = new Audio("sounds/tom-3.mp3");
       tom3.play();
       break;
     case "d":
-      var tom4 = new Audio(
-        "/my web code/Drum Kit Starting Files/sounds/tom-4.mp3"
-      );
+      var tom4 = new Audio("sounds/tom-4.mp3");
       tom4.play();
       break;
     case "j":
-      var snare = new Audio(
-        "/my web code/Drum Kit Starting Files/sounds/snare.mp3"
-      );
+      var snare = new Audio("sounds/snare.mp3");
       snare.play();
       break;
     case "k":
-      var crash = new Audio(
-        "/my web code/Drum Kit Starting Files/sounds/crash.mp3"
-      );
+      var crash = new Audio("sounds/crash.mp3");
       crash.play();
       break;
     case "l":
-      var kick_bass = new Audio(
-        "/my web code/Drum Kit Starting Files/sounds/kick-bass.mp3"
-      );
+      var kick_bass = new Audio("sounds/kick-bass.mp3");
       kick_bass.play();
       break;
     default:
@@ -62,13 +48,10 @@ function makesound(key) {
   }
 }
 function buttonAnimation(currentKey) {
-  currentKey=currentKey.toLowerCase();
-   var activeButton=document.querySelector("."+currentKey);
-    activeButton.classList.add("pressed");
-  setTimeout(function() {
+  currentKey = currentKey.toLowerCase();
+  var activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
+  setTimeout(function () {
     activeButton.classList.remove("pressed");
-      
   }, 100);
-
-    
 }
